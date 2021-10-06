9d98add0aef40cd0d73030157fe4b1fb60fd68d3
RainbowWallDev <91648958+RainbowWallDev@users.noreply.github.com>
2021-10-06 11:17:33 +0800

    Update rainbowwall.sol

--- rainbowwall.sol
+++ rainbowwall.sol
@@ -2049,77 +2049,6 @@ abstract contract ERC20Burnable is Context, ERC20 {
     }
 }
 
-/// [MIT License]
-/// @title Base64
-/// @notice Provides a function for encoding some bytes in base64
-/// @author Brecht Devos <brecht@loopring.org>
-library Base64 {
-    bytes internal constant TABLE =
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-
-    /// @notice Encodes some bytes to the base64 representation
-    function encode(bytes memory data) internal pure returns (string memory) {
-        uint256 len = data.length;
-        if (len == 0) return "";
-
-        // multiply by 4/3 rounded up
-        uint256 encodedLen = 4 * ((len + 2) / 3);
-
-        // Add some extra buffer at the end
-        bytes memory result = new bytes(encodedLen + 32);
-
-        bytes memory table = TABLE;
-
-        assembly {
-            let tablePtr := add(table, 1)
-            let resultPtr := add(result, 32)
-
-            for {
-                let i := 0
-            } lt(i, len) {
-
-            } {
-                i := add(i, 3)
-                let input := and(mload(add(data, i)), 0xffffff)
-
-                let out := mload(add(tablePtr, and(shr(18, input), 0x3F)))
-                out := shl(8, out)
-                out := add(
-                    out,
-                    and(mload(add(tablePtr, and(shr(12, input), 0x3F))), 0xFF)
-                )
-                out := shl(8, out)
-                out := add(
-                    out,
-                    and(mload(add(tablePtr, and(shr(6, input), 0x3F))), 0xFF)
-                )
-                out := shl(8, out)
-                out := add(
-                    out,
-                    and(mload(add(tablePtr, and(input, 0x3F))), 0xFF)
-                )
-                out := shl(224, out)
-
-                mstore(resultPtr, out)
-
-                resultPtr := add(resultPtr, 4)
-            }
-
-            switch mod(len, 3)
-            case 1 {
-                mstore(sub(resultPtr, 2), shl(240, 0x3d3d))
-            }
-            case 2 {
-                mstore(sub(resultPtr, 1), shl(248, 0x3d))
-            }
-
-            mstore(result, encodedLen)
-        }
-
-        return string(result);
-    }
-}
-
 contract RainbowWallToken is ERC20Burnable, ReentrancyGuard {
     uint256 initAmount = 100 * 10**decimals();
 
@@ -2130,6 +2059,10 @@ contract RainbowWallToken is ERC20Burnable, ReentrancyGuard {
     uint256 tokenId;
 
     uint256 feeBase = 100;
+
+    event In(uint256 amount);
+
+    event Out(uint256 amount);
 
     constructor(
         string memory _name,
@@ -2154,23 +2087,31 @@ contract RainbowWallToken is ERC20Burnable, ReentrancyGuard {
             (address(this).balance + initETHAmount) * balanceOf(address(this));
     }
 
+    function getAmountOutInternal(uint256 _ethAmount) internal view returns (uint256) {
+        uint256 qty = (_ethAmount * balanceOf(address(this))) /
+            (address(this).balance + initETHAmount);
+
+        if (qty > balanceOf(address(this))) qty = balanceOf(address(this));
+        return qty;
+    }    
+
     function getAmountOut(uint256 _ethAmount) public view returns (uint256) {
-        uint256 qty = balanceOf(address(this)) -
-            (k() / (address(this).balance + initETHAmount + _ethAmount));
+        uint256 qty = (_ethAmount * balanceOf(address(this))) /
+            (address(this).balance + initETHAmount + _ethAmount);
+
         if (qty > balanceOf(address(this))) qty = balanceOf(address(this));
         return qty;
     }
 
     function getAmountIn(uint256 _amount) public view returns (uint256) {
-        uint256 qty = address(this).balance +
-            initETHAmount -
-            (k() / (balanceOf(address(this)) + _amount));
+        uint256 qty = (_amount * (address(this).balance + initETHAmount)) /
+            (balanceOf(address(this)) + _amount);
         if (qty > address(this).balance) qty = address(this).balance;
         return qty;
     }
 
     function buy(address payable _to) external payable nonReentrant {
-        uint256 amount = getAmountOut(msg.value);
+        uint256 amount = getAmountOutInternal(msg.value);
         uint256 fee = amount / feeBase;
         _transfer(address(this), rainbowWallProtocol.ownerOf(tokenId), fee);
         _transfer(address(this), _to, amount - fee);
